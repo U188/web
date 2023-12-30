1a5c6fa20e23017fd61337e5e89f26da830dee30
U188 <54225629+U188@users.noreply.github.com>
2023-12-30 13:53:21 +0800

    更新 ml.js

---
+++ ml.js
@@ -0,0 +1,11 @@
+[rewrite_local]
+https://api.mlinkapp.cc/v1/user url script-response-body https://raw.githubusercontent.com/u188/web/main/ml.js
+
+[mitm]
+hostname = api.mlinkapp.cc
+
+*/
+let obj = JSON.parse($response.body);
+obj.level = 3; 
+
+$done({ body: JSON.stringify(obj) });
