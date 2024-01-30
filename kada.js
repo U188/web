86dcf4bde83ef5e33d87b070a6230963d7860ace
U188 <54225629+U188@users.noreply.github.com>
2024-01-31 07:47:05 +0800

    更新 kada.js

---
+++ kada.js
@@ -0,0 +1,16 @@
+
+[rewrite_local]
+https:\/\/service.hhdd.com\/user\/getUserDetail.json url script-response-body https://raw.githubusercontent.com/u188/aoyou/main/kada.js
+
+[mitm]
+hostname = service.hhdd.com
+
+var obj = JSON.parse($response.body);
+obj.data.gameLevel = 1;
+obj.data.vipInfoV2.vipStatus=1;
+obj.data.vipInfo.vipType=1;
+obj.data.vipInfo.vipStatus= 1;
+obj.data.vipInfo.vipType= 1;
+
+
+$done({ body: JSON.stringify(obj) });
