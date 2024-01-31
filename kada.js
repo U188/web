a683d47b0e205d68202e3bbfe8b5f60e03ecd1a6
U188 <54225629+U188@users.noreply.github.com>
2024-01-31 13:12:40 +0800

    更新 kada.js

--- kada.js
+++ kada.js
@@ -17,58 +17,61 @@ obj={
   "data" : {
     "readInfo" : {
       "readNumber" : 0,
-      "totalCount" : 2,
+      "totalCount" : 3,
       "honorNumber" : 0,
       "allRanking" : 0,
-      "readTimes" : 88,
+      "readTimes" : 90,
       "totalWords" : 0,
       "ranking" : 0,
-      "storyTime" : 0,
-      "storyCount" : 0
+      "storyTime" : 34185,
+      "storyCount" : 2
     },
-    "gameLevel" : 2,
+    "gameLevel" : 1,
     "vipInfoV2" : {
       "withholdingTime" : 0,
       "remainingDays" : 0,
       "payFlag" : 0,
       "channel" : 0,
-      "vipStatus" : 2,
+      "vipStatus" : 0,
       "signStatus" : 0,
       "priority" : 0,
       "remindExpire" : 0,
-      "level" : 2,
+      "level" : 0,
       "remindExpireDays" : 0,
       "endTime" : 0,
       "withholdingOrderPrice" : 0,
       "withholdingPayPrice" : 0,
       "beginTime" : 0,
       "remindRenewal" : 0,
-      "status" : 2,
-      "vipType" : 2
+      "status" : 0,
+      "vipType" : 0
     },
     "medalList" : null,
-    "medalNum" : 3,
+    "medalNum" : 5,
     "coinInfo" : {
       "consumeCoin" : 0,
-      "coin" : 100,
+      "coin" : 0,
       "voucherCoin" : 0
     },
     "createDays" : 1,
-    "vipList" : ["svip","svip"
+    "currentTime" : 1706677172136,
+    "newUserDate" : 7,
+    "vipList" : [
+
     ],
     "elfInfo" : {
-      "level" : 2
+      "level" : 1
     },
     "userInfo" : {
       "birthday" : "2021-08-31",
-      "status" : 2,
+      "status" : 1,
       "schoolId" : -100,
       "appHeadVersion" : 0,
       "inactiveDays" : 0,
       "schoolAwardStatus" : 0,
       "wechatOfficialSubscribe" : 0,
       "baseTag" : 4098,
-      "subscribePlan" : false,
+      "subscribePlan" : true,
       "toast" : true,
       "collectId" : 700001,
       "headUrl" : "https://image.hhdd.com/user/systemHead/0/1/0/boy2.png",
@@ -81,27 +84,27 @@ obj={
       "createDate" : 1706656529000,
       "schoolPeriod" : 0,
       "ageTypeFromBirthday" : 4,
-      "isUserSet" : 100,
+      "isUserSet" : 2,
       "userId" : 63068164
     },
     "vipInfo" : {
       "withholdingTime" : 0,
-      "remainingDays" : 2,
+      "remainingDays" : 0,
       "payFlag" : 0,
       "channel" : 0,
-      "vipStatus" : 2,
-      "signStatus" : 2,
+      "vipStatus" : 1,
+      "signStatus" : 0,
       "priority" : 0,
-      "remindExpire" : 100,
-      "level" : 4,
-      "remindExpireDays" : 100,
+      "remindExpire" : 0,
+      "level" : 0,
+      "remindExpireDays" : 0,
       "endTime" : 0,
       "withholdingOrderPrice" : 0,
       "withholdingPayPrice" : 0,
       "beginTime" : 0,
       "remindRenewal" : 0,
-      "status" : 2,
-      "vipType" : 2
+      "status" : 0,
+      "vipType" : 1
     }
   },
   "code" : 200
