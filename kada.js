5cf524589b88be5f0782478c74146777acb75780
U188 <54225629+U188@users.noreply.github.com>
2024-01-31 13:42:45 +0800

    更新 kada.js

--- kada.js
+++ kada.js
@@ -17,61 +17,25 @@ obj={
   "data" : {
     "readInfo" : {
       "readNumber" : 0,
-      "totalCount" : 3,
+      "totalCount" : 2,
       "honorNumber" : 0,
       "allRanking" : 0,
-      "readTimes" : 90,
+      "readTimes" : 88,
       "totalWords" : 0,
       "ranking" : 0,
-      "storyTime" : 34185,
-      "storyCount" : 2
-    },
-    "gameLevel" : 1,
-    "vipInfoV2" : {
-      "withholdingTime" : 0,
-      "remainingDays" : 0,
-      "payFlag" : 0,
-      "channel" : 0,
-      "vipStatus" : 0,
-      "signStatus" : 0,
-      "priority" : 0,
-      "remindExpire" : 0,
-      "level" : 0,
-      "remindExpireDays" : 0,
-      "endTime" : 0,
-      "withholdingOrderPrice" : 0,
-      "withholdingPayPrice" : 0,
-      "beginTime" : 0,
-      "remindRenewal" : 0,
-      "status" : 0,
-      "vipType" : 0
-    },
-    "medalList" : null,
-    "medalNum" : 5,
-    "coinInfo" : {
-      "consumeCoin" : 0,
-      "coin" : 0,
-      "voucherCoin" : 0
-    },
-    "createDays" : 1,
-    "currentTime" : 1706677172136,
-    "newUserDate" : 7,
-    "vipList" : [
-
-    ],
-    "elfInfo" : {
-      "level" : 1
+      "storyTime" : 0,
+      "storyCount" : 0
     },
     "userInfo" : {
       "birthday" : "2021-08-31",
-      "status" : 1,
+      "status" : 6,
       "schoolId" : -100,
       "appHeadVersion" : 0,
       "inactiveDays" : 0,
       "schoolAwardStatus" : 0,
       "wechatOfficialSubscribe" : 0,
       "baseTag" : 4098,
-      "subscribePlan" : true,
+      "subscribePlan" : "svip年卡",
       "toast" : true,
       "collectId" : 700001,
       "headUrl" : "https://image.hhdd.com/user/systemHead/0/1/0/boy2.png",
@@ -84,27 +48,62 @@ obj={
       "createDate" : 1706656529000,
       "schoolPeriod" : 0,
       "ageTypeFromBirthday" : 4,
-      "isUserSet" : 2,
+      "isUserSet" : 100,
       "userId" : 63068164
     },
     "vipInfo" : {
       "withholdingTime" : 0,
+      "remainingDays" : 2,
+      "payFlag" : 0,
+      "channel" : 0,
+      "vipStatus" : 6,
+      "signStatus" : 6,
+      "priority" : 0,
+      "remindExpire" : 100,
+      "level" : 4,
+      "remindExpireDays" : 100,
+      "endTime" : 0,
+      "withholdingOrderPrice" : 0,
+      "withholdingPayPrice" : 0,
+      "beginTime" : 0,
+      "remindRenewal" : 0,
+      "status" : 6,
+      "vipType" : 6
+    },
+    "medalList" : null,
+    "medalNum" : 3,
+    "coinInfo" : {
+      "consumeCoin" : 0,
+      "coin" : 100,
+      "voucherCoin" : 0
+    },
+    "createDays" : 1,
+    "newUserDate" : 7,
+    "vipList" : [
+
+    ],
+    "elfInfo" : {
+      "level" : 6
+    },
+    "gameLevel" : 6,
+    "vipInfoV2" : {
+      "withholdingTime" : 0,
       "remainingDays" : 0,
       "payFlag" : 0,
       "channel" : 0,
-      "vipStatus" : 1,
+      "vipStatus" : 6,
       "signStatus" : 0,
       "priority" : 0,
       "remindExpire" : 0,
-      "level" : 0,
+      "level" : 6,
       "remindExpireDays" : 0,
       "endTime" : 0,
       "withholdingOrderPrice" : 0,
       "withholdingPayPrice" : 0,
       "beginTime" : 0,
       "remindRenewal" : 0,
-      "status" : 0,
-      "vipType" : 1
+      "status" : 6,
+      "vipType" : 6
     }
   },
   "code" : 200
