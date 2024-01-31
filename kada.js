4e81c7d8975ccdfd0b275718d514fceb0d393cba
U188 <54225629+U188@users.noreply.github.com>
2024-01-31 11:27:08 +0800

    更新 kada.js

--- kada.js
+++ kada.js
@@ -13,11 +13,103 @@ hostname = service.hhdd.com:443
 
 
 var obj = JSON.parse($response.body);
-obj.data.gameLevel = 3;
-obj.data.vipInfoV2.vipStatus=3;
-obj.data.vipInfo.vipType=3;
-obj.data.vipInfo.vipStatus= 3;
-obj.data.vipInfo.vipType= 3;
+obj={
+  "msg" : "成功",
+  "data" : {
+    "readInfo" : {
+      "readNumber" : 0,
+      "totalCount" : 2,
+      "honorNumber" : 0,
+      "allRanking" : 0,
+      "readTimes" : 88,
+      "totalWords" : 0,
+      "ranking" : 0,
+      "storyTime" : 0,
+      "storyCount" : 0
+    },
+    "gameLevel" : 1,
+    "vipInfoV2" : {
+      "withholdingTime" : 0,
+      "remainingDays" : 0,
+      "payFlag" : 0,
+      "channel" : 0,
+      "vipStatus" : 1,
+      "signStatus" : 0,
+      "priority" : 0,
+      "remindExpire" : 0,
+      "level" : 1,
+      "remindExpireDays" : 0,
+      "endTime" : 0,
+      "withholdingOrderPrice" : 0,
+      "withholdingPayPrice" : 0,
+      "beginTime" : 0,
+      "remindRenewal" : 0,
+      "status" : 0,
+      "vipType" : 1
+    },
+    "medalList" : null,
+    "medalNum" : 3,
+    "coinInfo" : {
+      "consumeCoin" : 0,
+      "coin" : 0,
+      "voucherCoin" : 0
+    },
+    "createDays" : 1,
+    "currentTime" : 1706671257842,
+    "newUserDate" : 7,
+    "vipList" : ["svip"
+
+    ],
+    "elfInfo" : {
+      "level" : 2
+    },
+    "userInfo" : {
+      "birthday" : "2021-08-31",
+      "status" : 2,
+      "schoolId" : -100,
+      "appHeadVersion" : 0,
+      "inactiveDays" : 0,
+      "schoolAwardStatus" : 0,
+      "wechatOfficialSubscribe" : 0,
+      "baseTag" : 4098,
+      "subscribePlan" : false,
+      "toast" : true,
+      "collectId" : 700001,
+      "headUrl" : "https://image.hhdd.com/user/systemHead/0/1/0/boy2.png",
+      "gender" : "m",
+      "level" : 10,
+      "nick" : "嘻嘻",
+      "ageType" : 4,
+      "loginName" : "17628608281",
+      "age" : 3,
+      "createDate" : 1706656529000,
+      "schoolPeriod" : 0,
+      "ageTypeFromBirthday" : 4,
+      "isUserSet" : 1,
+      "userId" : 63068164
+    },
+    "vipInfo" : {
+      "withholdingTime" : 0,
+      "remainingDays" : 0,
+      "payFlag" : 0,
+      "channel" : 0,
+      "vipStatus" : 1,
+      "signStatus" : 1,
+      "priority" : 0,
+      "remindExpire" : 0,
+      "level" : 4,
+      "remindExpireDays" : 0,
+      "endTime" : 0,
+      "withholdingOrderPrice" : 0,
+      "withholdingPayPrice" : 0,
+      "beginTime" : 0,
+      "remindRenewal" : 0,
+      "status" : 1,
+      "vipType" : 1
+    }
+  },
+  "code" : 200
+}
 
 
 $done({ body: JSON.stringify(obj) });
