0eb7898141b3588d647b5ca2b653f59f0ecf1eaa
U188 <54225629+U188@users.noreply.github.com>
2024-02-03 21:28:00 +0800

    更新 ml.js

--- ml.js
+++ ml.js
@@ -13,6 +13,6 @@ hostname = api-jiaoyu-brain.vas.lutongnet.com:8887
 
 
 var obj = JSON.parse($response.body);
-obj.userInfo.product = "nldmx_iOS_VIP0001";
+obj.userInfo.product = "VIP";
 
 $done({ body: JSON.stringify(obj) });
