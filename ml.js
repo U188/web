819a304a023ca6a779af6a758bc4db9edb3ab7fe
U188 <54225629+U188@users.noreply.github.com>
2024-02-03 21:06:20 +0800

    更新 ml.js

--- ml.js
+++ ml.js
@@ -7,12 +7,12 @@
 https://api-jiaoyu-brain.vas.lutongnet.com:8887/lutong-user-api/user/get-user-info url script-response-body https://raw.githubusercontent.com/u188/web/main/ml.js
 
 [mitm]
-hostname = api-jiaoyu-brain.vas.lutongnet.com
+hostname = api-jiaoyu-brain.vas.lutongnet.com:8887
 
 *************************************/
 
 
 var obj = JSON.parse($response.body);
-obj.userInfo.product = "VIP0008";
+obj.userInfo.product = "nldmx_iOS_VIP0001";
 
 $done({ body: JSON.stringify(obj) });
