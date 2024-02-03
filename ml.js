c87c78dbae17b9abbde9dca72bcde2dacdfee964
U188 <54225629+U188@users.noreply.github.com>
2024-02-03 08:54:29 +0800

    更新 ml.js

--- ml.js
+++ ml.js
@@ -4,16 +4,15 @@
 
 
 [rewrite_local]
-https:\/\/api.mlinkapp.cc\/v1\/user url script-response-body https://raw.githubusercontent.com/u188/web/main/ml.js
+https://api-jiaoyu-brain.vas.lutongnet.com:8887/lutong-user-api/user/get-user-info url script-response-body https://raw.githubusercontent.com/u188/web/main/ml.js
 
 [mitm]
-hostname = api.mlinkapp.cc
+hostname = api-jiaoyu-brain.vas.lutongnet.com
 
 *************************************/
 
 
 var obj = JSON.parse($response.body);
-obj.data.level = 3;
-obj.data.expireTime = 4102311455000;
+obj.userInfo.product = "nldmx_iOS_VIP";
 
 $done({ body: JSON.stringify(obj) });
